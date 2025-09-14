149bdd0c2ac92ee9c056020102ebebc502062cef
shourya-shivhare <shouryashivhare2711@gmail.com>
2025-09-15 02:57:59 +0530

    prototype 1 (added HTML , CSS and javascript files)
    created a prototype by adding the following features using Leaflit.js :  
     1.  add map form internet server OpenStreetMap.
     2. added a few markers.
     3. connected those markers using routes and polyline.
    (Photo of the prototype 1).

---
+++ frontend/script.js
@@ -0,0 +1,45 @@
+const map = L.map('map').setView([31.818, 75.207], 13);
+
+
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '© OpenStreetMap contributors'
+}).addTo(map);
+
+const busStops = [
+  { name: "Batala Bus Stand", coords: [31.818, 75.207] },
+  { name: "Railway Road Stop", coords: [31.820, 75.215] },
+  { name: "City Market Stop", coords: [31.825, 75.225] },
+  { name: "Civil Hospital Stop", coords: [31.822, 75.212] },
+  { name: "Model Town Stop", coords: [31.824, 75.218] },
+  { name: "Gurdwara Bunga Stop", coords: [31.827, 75.222] },
+  { name: "Sadar Bazar Stop", coords: [31.829, 75.228] }
+];
+
+busStops.forEach(stop => {
+    L.marker(stop.coords).addTo(map)
+        .bindPopup(`${stop.name}` , {
+            closeButton : false,
+        })
+})
+
+const route_1 = [
+    [31.818, 75.207],
+    [31.820, 75.215],
+    [31.825, 75.225]
+];
+
+L.polyline(route_1, { color: 'blue' }).addTo(map)
+  .bindPopup("Bus Route 1");
+
+const route_2 = [
+    [31.822, 75.212],
+    [31.824, 75.218],
+    [31.827, 75.222],
+    [31.829, 75.228] 
+];
+
+
+L.polyline(route_2, { color: 'green' }).addTo(map)
+  .bindPopup("Bus Route 2");
+
